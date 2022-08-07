0d8b83e5e5e4bb84242189d3a377b019413afda5
rachier-alal <rachiermark285@gmail.com>
2022-08-07 13:12:11 +0300

    tired af

--- dorsam_vue/src/views/HomeView.vue
+++ dorsam_vue/src/views/HomeView.vue
@@ -13,11 +13,11 @@
       <div class="column is-3" v-for="product in latestProducts" :key="product.id">
         <div class="box">
           <figure class="image mb-4">
-            <img :src="product.get_image" alt="">
+            <img :src="product.get_thumbnail" alt="no image to display">
           </figure>
           <h3 class="is-size-4">{{ product.name }}</h3>
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-          View Details
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View Details</router-link>
         </div>
       </div>
     </div>
