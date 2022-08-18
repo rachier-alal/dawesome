8b3f12aecdabe7416dd860385cc2c06fe244c783
rachier-alal <rachiermark285@gmail.com>
2022-08-18 21:56:43 +0300

    bedtime

---
+++ dorsam_vue/src/views/Checkout.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="columns is-multiline">
+        <div class="column is-2">
+            <h2 class="title">Checkout</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Checkout',
+    methods: {
+        getItemTotal(item) {
+            return item.quantity * item.product.price
+        },
+
+    },
+    data() {
+        return {
+            stripe: {},
+            card: {},
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: '',
+            address: '',
+            zipcode: '',
+            place: '',
+            errors: []
+        }
+    },
+    mounted() {
+        document.title = 'Checkout | Dawesome'
+
+        this.cart = this.$store.state.cart
+    },
+    computed: {
+        cartTotalPrice() {
+            return this.cart.items.reduce((acc, curVal) => {
+                return acc += curVal.product.price * curVal.quantity
+            }, 0)
+        },
+        cartTotalLength() {
+            return this.cart.items.reduce((acc, curVal) => {
+                return acc += curVal.quantity
+            }, 0)
+        }
+    }
+}
+</script>
